df6b265e5e4404f4b0125942061ba493714c4d17
2concrete <danielpeace654@gmail.com>
2026-01-27 23:34:10 +0800

    condensed to WordDisplay.tsx

---
+++ app/components/WordDisplay.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { useMutation } from "convex/react";
+import { useSearchParams } from "next/navigation";
+import { connection } from "next/server";
+import { useEffect, useState } from "react";
+
+const WordDisplay = () => {
+  const [word, setWord] = useState<string>("");
+  const params = useSearchParams();
+  const textDisplay = params.get("text");
+  const logParam = useMutation(api.visits.logParam);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWord(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    window.location.href = `http://localhost:3000?text=${word}`;
+  };
+
+  useEffect(() => {
+    if (textDisplay) {
+      logParam({ param: textDisplay });
+    }
+  }, [textDisplay, logParam]);
+
+  return (
+    <div className="flex justify-center items-center">
+      <div>
+        <main>
+          <div className="text-2xl font-[Inter] flex items-center justify-center min-h-screen">
+            <form onSubmit={handleSubmit}>
+              <input
+                onChange={handleChange}
+                placeholder={textDisplay ? textDisplay : "enter wrds"}
+                className="outline-none w-30 placeholder-white"
+                value={word}
+              />
+            </form>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default WordDisplay;
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,12 @@
-"use client";
+import { Suspense } from "react";
+import WordDisplay from "./components/WordDisplay";
 
-import { api } from "@/convex/_generated/api";
-import { useMutation } from "convex/react";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-
-export default function Home() {
-  const params = useSearchParams();
-  const textDisplay = params.get("text");
-  const logParam = useMutation(api.visits.logParam);
-
-  useEffect(() => {
-    if (textDisplay) {
-      logParam({ param: textDisplay });
-    }
-  }, [textDisplay, logParam]);
-
+const Home = () => {
   return (
-    <div>
-      <main>
-        <div className="text-2xl font-[Inter] flex items-center justify-center min-h-screen">
-          <p>{textDisplay ? textDisplay : "empty"}</p>
-        </div>
-      </main>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <WordDisplay />
+    </Suspense>
   );
-}
+};
+
+export default Home;
