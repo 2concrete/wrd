98c4aa395cb15e8b2714ec7fe118f7be638f8404
2concrete <danielpeace654@gmail.com>
2026-01-28 21:54:18 +0800

    background only on desktop

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,27 @@
 import { Suspense } from "react";
 import WordDisplay from "./components/WordDisplay";
 import LightPillar from "./components/LightPillar";
+import { isDesktop } from "react-device-detect";
 
 const Home = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <LightPillar
-        topColor="#fffffff"
-        bottomColor="#000000"
-        intensity={1}
-        rotationSpeed={0.3}
-        glowAmount={0.002}
-        pillarWidth={3}
-        pillarHeight={0.4}
-        noiseIntensity={0.5}
-        pillarRotation={25}
-        interactive={false}
-        mixBlendMode="screen"
-        quality="high"
-      />
+      {isDesktop && (
+        <LightPillar
+          topColor="#fffffff"
+          bottomColor="#000000"
+          intensity={1}
+          rotationSpeed={0.3}
+          glowAmount={0.002}
+          pillarWidth={3}
+          pillarHeight={0.4}
+          noiseIntensity={0.5}
+          pillarRotation={25}
+          interactive={false}
+          mixBlendMode="screen"
+          quality="high"
+        />
+      )}
       <WordDisplay />
     </Suspense>
   );
